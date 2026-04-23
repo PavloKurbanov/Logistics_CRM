37c5d3683a4fb01fec1769757ca60ccc26f9d2f1
Pavlo <kurbanovpavlo@gmail.com>
2026-04-23 23:24:28 +0300

    reate ClientServiceImpl

--- src/main/java/org/example/logistics_crm/order/dto/CreateOrderRequestDTO.java
+++ src/main/java/org/example/logistics_crm/order/dto/CreateOrderRequestDTO.java
@@ -1,15 +1,29 @@
 package org.example.logistics_crm.order.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record CreateOrderRequestDTO(
+        @NotBlank
         Long senderClientId,
+        @NotNull
         Long receiverClientId,
+        @NotBlank
         String pickupAddress,
+        @NotBlank
         String deliveryAddress,
+        @NotNull
+        @DecimalMin("0")
         BigDecimal price,
+        @NotNull
+        @Min(1)
         Double weight,
+        @NotNull
         LocalDateTime deliveryDate
 ) {
 }
