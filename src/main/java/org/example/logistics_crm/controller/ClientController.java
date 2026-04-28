1903459af93fbe2f25076a08b6671e134ff0f103
Pavlo <kurbanovpavlo@gmail.com>
2026-04-28 16:00:20 +0300

    Add endpoints for changing phone number and email in ClientController

--- src/main/java/org/example/logistics_crm/controller/ClientController.java
+++ src/main/java/org/example/logistics_crm/controller/ClientController.java
@@ -1,9 +1,7 @@
 package org.example.logistics_crm.controller;
 
 import jakarta.validation.Valid;
-import org.example.logistics_crm.dto.client.request.ChangeClientPasswordRequestDTO;
-import org.example.logistics_crm.dto.client.request.ClientSearchRequestDTO;
-import org.example.logistics_crm.dto.client.request.CreateClientRequestDTO;
+import org.example.logistics_crm.dto.client.request.*;
 import org.example.logistics_crm.dto.client.response.ClientDetailsResponseDTO;
 import org.example.logistics_crm.dto.client.response.ClientListResponseDTO;
 import org.example.logistics_crm.service.client.AccountClientService;
@@ -13,8 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/clients")
@@ -63,9 +59,23 @@ public class ClientController {
     }
 
     @PutMapping("/{id}/password")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(HttpStatus.OK)
     public ClientDetailsResponseDTO changePassword(
             @PathVariable Long id, @Valid @RequestBody ChangeClientPasswordRequestDTO request) {
         return accountClientService.changePassword(id, request);
+    }
+
+    @PutMapping("/{id}/phoneNumber")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ClientDetailsResponseDTO changePhoneNumber(
+            @PathVariable Long id, @Valid @RequestBody ChangeClientPhoneNumberDTO request) {
+        return accountClientService.changePhoneNumber(id, request);
+    }
+
+    @PutMapping("/{id}/email")
+    @ResponseStatus(HttpStatus.OK)
+    public ClientDetailsResponseDTO changeEmail(
+            @PathVariable Long id, @Valid @RequestBody ChangeClientEmailRequestDTO request) {
+        return accountClientService.changeEmail(id, request);
     }
 }
