5508db06f3e8dbdfd5262f11b09032376fdc8541
Pavlo <kurbanovpavlo@gmail.com>
2026-05-20 21:36:06 +0300

    Enhance security configuration by adding user detail services and refining authentication provider setup

--- src/main/java/org/example/logistics_crm/config/SecurityConfig.java
+++ src/main/java/org/example/logistics_crm/config/SecurityConfig.java
@@ -1,14 +1,37 @@
 package org.example.logistics_crm.config;
 
+import org.example.logistics_crm.repository.ClientRepository;
+import org.example.logistics_crm.repository.UserRepository;
+import org.example.logistics_crm.security.service.ClientUserDetailsService;
+import org.example.logistics_crm.security.service.StaffUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder, UserRepository userRepository, ClientRepository clientRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+        this.clientRepository = clientRepository;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,15 +41,39 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         // відкриваємо доступ до всіх endpoint-ів
-                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
 
                         // усе інше теж можна тимчасово відкрити
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
 
                 // basic auth можна залишити, але зараз не обов’язково
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider staffAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(staffUserDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider clientAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(clientUserDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public ClientUserDetailsService clientUserDetailsService() {
+        return new ClientUserDetailsService(clientRepository);
+    }
+
+    @Bean
+    public StaffUserDetailsService staffUserDetailsService() {
+        return new StaffUserDetailsService(userRepository);
     }
 }
